c9567258aba52b80492ab0268333a5f18cefef6d
zhangyongwang <349368992@qq.com>
2018-07-31 10:39:11 +0800

    封装函数、代码优化

--- js/main.js
+++ js/main.js
@@ -1,51 +1,75 @@
 let $buttons = $('#buttonWrapper > button')
 let $slides = $('#slides')
-let $images = $slides.children('img') 
-let $firstCopy = $images.eq(0).clone(true)
-let $lastCopy  =$images.eq($images.length-1).clone(true)
+let $images = $slides.children('img')
+let current = 0
 
-$slides.append($firstCopy)
-$slides.prepend($lastCopy)
+makeFakeSlides()
+$slides.css({
+	transform: 'translateX(-400px)'
+})
+bindEvents()
 
-$slides.css({transform:'translateX(-400px)'})
 
-let current = 0 
-$buttons.eq(0).on('click',function(){
-	if(current == 2){
-		console.log('说明你是从最后一张到第一张')
-		$slides.css({transform:'translateX(-1600px'})
-			.one('transitionend',function(){
-				console.log('动画完毕')
-				$slides.hide()
-				.offset()
-				$slides.css({transform:'translateX(-400px'})
-				.show()
+
+function makeFakeSlides() {
+	let $firstCopy = $images.eq(0).clone(true)
+	let $lastCopy = $images.eq($images.length - 1).clone(true)
+
+	$slides.append($firstCopy)
+	$slides.prepend($lastCopy)
+}
+function bindEvents() {
+	$buttons.eq(0).on('click', function() {
+		if (current == 2) {
+			console.log('说明你是从最后一张到第一张')
+			$slides.css({
+					transform: 'translateX(-1600px'
+				})
+				.one('transitionend', function() {
+					console.log('动画完毕')
+					$slides.hide()
+						.offset()
+					$slides.css({
+							transform: 'translateX(-400px'
+						})
+						.show()
+				})
+		} else {
+			$slides.css({
+				transform: 'translateX(-400px'
 			})
-	}else{
-		$slides.css({transform:'translateX(-400px'})
-	}
-	
-	current =  0
-})
+		}
 
-$buttons.eq(1).on('click',function(){
-	$slides.css({transform:'translateX(-800px'})
-	current  = 1
-})
+		current = 0
+	})
 
-$buttons.eq(2).on('click',function(){
-	if(current == 0 ){
-		console.log('说明你是从第一张到最后一张')
-		$slides.css({transform:'translateX(0px'})
-			.one('transitionend',function(){
-				console.log('动画完毕')
-				$slides.hide()
-				.offset()
-				$slides.css({transform:'translateX(-1200px'})
-				.show()
+	$buttons.eq(1).on('click', function() {
+		$slides.css({
+			transform: 'translateX(-800px'
+		})
+		current = 1
+	})
+
+	$buttons.eq(2).on('click', function() {
+		if (current == 0) {
+			console.log('说明你是从第一张到最后一张')
+			$slides.css({
+					transform: 'translateX(0px'
+				})
+				.one('transitionend', function() {
+					console.log('动画完毕')
+					$slides.hide()
+						.offset()
+					$slides.css({
+							transform: 'translateX(-1200px'
+						})
+						.show()
+				})
+		} else {
+			$slides.css({
+				transform: 'translateX(-1200px'
 			})
-	}else{
-		$slides.css({transform:'translateX(-1200px'})
-	}
-	current = 2 
-})
+		}
+		current = 2
+	})
+}
